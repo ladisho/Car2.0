e4052b8e97d95f154a3f818ccd65fa4b0823617c
ladisho <20084356@mail.wit.ie>
2021-10-07 14:48:41 +0100

    car view refactored

--- src/main/kotlin/org/wit/car/console/main/main.kt
+++ src/main/kotlin/org/wit/car/console/main/main.kt
@@ -7,13 +7,6 @@ import org.wit.car.console.views.CarView
 
 private val logger = KotlinLogging.logger {}
 
-//var model : String = ""
-//var brand : String = ""
-//var year : Int = 0
-//var plateNumber : String = ""
-
-//var car = CarModel()
-
 val cars = CarMemStore()
 val carView = CarView()
 
@@ -29,7 +22,7 @@ fun main(args: Array<String>){
             1 -> addCar()
             2 -> updateCar()
             3 -> carView.listCars(cars)
-            4 -> searchCars()
+            4 -> searchCar()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -38,137 +31,48 @@ fun main(args: Array<String>){
     logger.info { "Shutting Down Car Console App" }
 }
 
-//fun menu() : Int {
-//
-//    val option : Int
-//    var input: String? = null
-//
-//    println("Main Menu")
-//    println(" 1. Add Car")
-//    println(" 2. Update a Car")
-//    println(" 3. List All Cars")
-//    println(" 4. Search Cars")
-//    println("-1. Exit")
-//    println()
-//    print("Enter an integer : ")
-//    input = readLine()!!
-//    option = if (input.toIntOrNull() != null && !input.isEmpty())
-//        input.toInt()
-//    else
-//        -9
-//
-//    return option
-//}
 
 fun addCar(){
-
-
     val aCar = CarModel()
-    println("Add Car")
-    println()
-    carView.addCarData(aCar)
-//    print("Enter the Model : ")
-//    aCar.model = readLine()!!
-//    print("Enter the Brand : ")
-//    aCar.brand = readLine()!!
-//    print("Enter the Year : ")
-//
-//    aCar.year = readLine()!!
-//    print("Enter the PlateNumber : ")
-//    aCar.plateNumber = readLine()!!
-//    if ( aCar.brand.isNotEmpty() && aCar.model.isNotEmpty() && aCar.plateNumber.isNotEmpty() && aCar.year.isNotEmpty()  ){
-//        aCar.id = cars.findAll().size.toLong()
-//        cars.create(aCar.copy())
-////        aCar.id++
-//        logger.info("Car Added : [ $aCar ]")
-//        println()
-//        println("You entered [ ${car.model} ] for model" + "\n"
-//                + "You entered [ ${car.brand} ] for brand" + "\n"
-//                + "You entered [ ${car.year} ] for year" + "\n"
-//                + "You entered [ ${car.plateNumber} ] for plateNumber"
-//        )
-//    }else{
-//        logger.info("Car Not Added")
-//    }
+
+    if (carView.addCarData(aCar))
+        cars.create(aCar)
+    else
+        logger.info("Car Not Added")
 
 }
 
 fun updateCar(){
-//    println("List Of All Cars")
-//    println(cars)
-//    println()
     carView.listCars(cars)
-    val id = carView.getId()
-    val aCar = search(id)
-//    carView.updateCarData(aCar.)
+    var searchId = carView.getId()
+    val aCar = search(searchId)
 
-//    println("Update Car")
-//    println()
-//    searchCar()
+    if(aCar != null) {
+        if(carView.updateCarData(aCar)) {
+            cars.update(aCar)
+            carView.showCar(aCar)
+            logger.info("Car Updated : [ $aCar ]")
+        }
+        else
+            logger.info("Car Not Updated")
+    }
+    else
+        println("Car Not Updated...")
 
 }
-
-//fun listAllCars(){
-//    println("List Of All Cars")
-//    println()
-////    cars.forEach { logger.info("${it}") }
-//    println(cars)
-//}
-
-//fun getId() : Long {
-//    val strId : String? // String to hold user input
-//    val searchId : Long // Long to hold converted id
-//    print("Enter id to Search/Update : ")
-//    strId = readLine()!!
-//    searchId = if (strId.toLongOrNull() != null && !strId.isEmpty())
-//        strId.toLong()
-//    else
-//        -9
-//    return searchId
-//}
 
 fun search(id: Long) : CarModel? {
     val foundCar: CarModel? = cars.findOne(id)
     return foundCar
 }
 
-fun searchCars(){
-    val id = carView.getId()
-    println(search(id))
+fun searchCar(){
+    val aCar = search(carView.getId())!!
+    carView.showCar(aCar)
 
 }
 
-//fun searchCar(){
-//
-//    if (aCar != null){
-//        print("Enter a new Model for [ ${aCar.model} ] : ")
-//        val mo = readLine()!!
-//        print("Enter a new Brand for [ ${aCar.brand} ] : ")
-//        val br = readLine()!!
-//        print("Enter a new Year for [ ${aCar.year} ] : ")
-//        val yr = readLine()!!
-//        print("Enter a new PlateNumber for [ ${aCar.plateNumber} ] : ")
-//        val pl = readLine()!!
-//        if (mo != "" && br != "" && pl != "" && yr != ""){
-//            aCar.model = mo
-//            aCar.brand = br
-//            aCar.year = yr
-//            aCar.plateNumber = pl
-//            println()
-//            println("You updated [ ${aCar.model} ] for model" + "\n"
-//                    + "You updated [ ${aCar.brand} ] for brand" + "\n"
-//                    + "You updated [ ${aCar.year} ] for year" + "\n"
-//                    + "You updated [ ${aCar.plateNumber} ] for plateNumber"
-//            )
-//
-//        }else{
-//            logger.info { "Car Not Updated" }
-//        }
-//
-//
-//    } else{
-//        println("Car Not Updated...")
-//    }
+
 
 
 
